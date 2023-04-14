493f473147acf82ab4a2d5ab0677f6187f9895d9
Elysia Love <elysia@love>
2023-04-14 14:26:11 +0200

    2 bit in, 4 bit out, [-1, 1] filter working correctly

--- src/test.py
+++ src/test.py
@@ -2,9 +2,14 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+def binstr2int(x):
+    bw = len(x)
+    if bw == 1:
+        return -int(x, 2)
+    return int(x[1:], 2) - int(x[0], 2) * 2**(bw - 1)
 
-# segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
-
+input = [-1, 1, 1, -2, 1, -1, 1, 0, 1, 0, 0, 0, 0, 0]
+output_expected = [0, 0, 1, -2, 0, 3, -3, 2, -2, 1, -1, 1, 0, 0]
 @cocotb.test()
 async def test_gbsha_top(dut):
     dut._log.info("start")
@@ -14,10 +19,11 @@ async def test_gbsha_top(dut):
     dut.rst.value = 1
     await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
-    dut.x_in.value = -1
-
     dut._log.info("checking...")
-    for i in range(10):
-        dut._log.info("check {}".format(i))
+    output = []
+    for x in input:
+        dut.x_in.value = x
         await ClockCycles(dut.clk, 1)
-        print(f"{dut.y_out.value.binstr}")
+        output.append(binstr2int(dut.y_out.value.binstr))
+    for (actual, expected) in zip(output, output_expected):
+        assert actual == expected
--- src/ttfir.v
+++ src/ttfir.v
@@ -2,9 +2,9 @@
 
 module gbsha_top #(parameter N_TAPS = 2,
                              BW_in = 2,
-                             BW_out = 2,
-                             BW_product = 2, // TODO: clarify required sum bitwidth
-                             BW_sum = 2
+                             BW_out = 4,
+                             BW_product = 3, // TODO: clarify required sum bitwidth
+                             BW_sum = 4
                              )
 (
   input [7:0] io_in,
@@ -12,19 +12,21 @@ module gbsha_top #(parameter N_TAPS = 2,
 );
     wire clk = io_in[0];
     wire reset = io_in[1];
-    wire [BW_in - 1:0] x_in = io_in[BW_in - 1 + 2:2];
-    wire [BW_out - 1:0] y_out;
+    wire signed [BW_in - 1:0] x_in = io_in[BW_in - 1 + 2:2];
+    wire signed [BW_out - 1:0] y_out;
     assign io_out[BW_out - 1:0] = y_out;
 
-    reg [BW_in - 1:0] x;
-    wire [BW_sum - 1:0] y;
-    wire [BW_product - 1:0] product [0:N_TAPS - 1];
-    reg [BW_sum - 1:0] sum [0:N_TAPS - 1];
+    reg signed [BW_in - 1:0] x;
+    wire signed [BW_sum - 1:0] y;
+    wire signed [BW_product - 1:0] product [0:N_TAPS - 1];
+    reg signed [BW_sum - 1:0] sum [0:N_TAPS - 1];
 
     always @(posedge clk) begin
         // if reset, set counter to 0
         if (reset) begin
             x <= 0;
+            sum[0] <= 0;
+            sum[1] <= 0;
         end else begin
             x <= x_in;
         end
@@ -36,10 +38,9 @@ module gbsha_top #(parameter N_TAPS = 2,
     end
 
     // TODO: use generate
-    assign product[0] = 2 * x;
-    assign product[1] = 1 * x;
+    assign product[0] = -x;
+    assign product[1] = x;
 
     assign y = sum[0];
-    // assign y_out = y[BW_sum - 1:BW_sum - BW_out];
-    assign y_out = x_in;
+    assign y_out = y[BW_sum - 1:BW_sum - BW_out];
 endmodule
