7c736586b203964cb08e706ec357bed40577d286
Elysia Love <elysia@love>
2023-04-17 15:32:20 +0200

    serdes: storing amplitudes, signs of inputs, coefficients

--- src/test.py
+++ src/test.py
@@ -8,8 +8,8 @@ N_TAPS = 5
 BW_in = 6
 BW_out = 8
 
-input = [1, 2, 3, 4, 5, 63, 62, 61, 60, 59, 58, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-input_data = [0, 0, 0, 0, 0, 63, 62, 61, 60, 59, 58, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+input = [1, 2, 3, 4, 5, 30, 31, 32, 33, 34, 35, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+input_data = [0, 0, 0, 0, 0, 30, 31, 32, 31, 30, 29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 output_expected = input_data[-N_TAPS:] + input_data[:-N_TAPS]
 @cocotb.test()
 async def test_gbsha_top(dut):
@@ -22,7 +22,7 @@ async def test_gbsha_top(dut):
     dut.rst.value = 0
     dut._log.info("checking...")
     for i, x in enumerate(input):
-        dut.x_in.value = x % 2**BW_in
+        dut.x_in.value = x
         await ClockCycles(dut.clk, 1)
         output_actual = dut.y_out.value.integer
-        print(f"{output_actual = }, expected = {output_expected[i] % 2**BW_out}")
+        print(f"{output_actual = }, expected = {output_expected[i]}")
--- src/ttfir.v
+++ src/ttfir.v
@@ -16,7 +16,7 @@ module gbsha_top #(parameter N_TAPS = 5,
     reg [3:0] coefficients_loaded;
 
     // 
-    wire [BW_in - 1:0] x_in = io_in[BW_in - 1 + 2:2];
+    wire signed [BW_in - 1:0] x_in = io_in[BW_in - 1 + 2:2];
     wire [BW_out - 1:0] y_out;
     assign io_out[BW_out - 1:0] = y_out;
     if (BW_out < 8)
@@ -25,6 +25,8 @@ module gbsha_top #(parameter N_TAPS = 5,
     // shift register
     reg [BW_in - 1:0] x [N_TAPS - 1: 0];
     reg [BW_in - 1:0] coefficient [N_TAPS - 1: 0];
+    reg [N_TAPS - 1:0] x_sign;
+    reg [N_TAPS - 1:0] coefficient_sign;
 
     always @(posedge clk) begin
         // initialize shift register with zeros
@@ -32,16 +34,27 @@ module gbsha_top #(parameter N_TAPS = 5,
             coefficients_loaded <= 0;
             for (integer i = 0; i < N_TAPS; i = i + 1) begin
                 x[i] <= 0;
+                x_sign[i] <= 0;
                 coefficient[i] <= 0;
+                coefficient_sign[i] <= 0;
             end                
         end else if (coefficients_loaded < N_TAPS) begin
-            coefficient[coefficients_loaded] <= x_in;
+            case(x_in[BW_in - 1])
+                1'b0: coefficient[coefficients_loaded] <= x_in;
+                2'b1: coefficient[coefficients_loaded] <= -x_in;
+            endcase
+            coefficient_sign[coefficients_loaded] = x_in[BW_in - 1];
             coefficients_loaded <= coefficients_loaded + 1;
         end else begin
-            x[0] <= x_in;
+            case(x_in[BW_in - 1])
+                1'b0: x[0] <= x_in;
+                1'b1: x[0] <= -x_in;
+            endcase
+            x_sign[0] <= x_in[BW_in - 1];
             for (integer i = 1; i < N_TAPS; i = i + 1) begin
                 x[i] <= x[i - 1];
-            end                
+                x_sign[i] <= x_sign[i - 1];
+            end
         end
     end
 
