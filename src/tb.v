fb08da2ba17d23d41fa0b40dcb725143a016ee62
Nora Le <Nora@Le>
2023-04-18 21:19:21 +0200

    Prelease: 6-tap FIR

--- src/tb.v
+++ src/tb.v
@@ -6,11 +6,17 @@ this testbench just instantiates the module and makes some convenient wires
 that can be driven / tested by the cocotb test.py
 */
 
-module tb (
-    // testbench is controlled by test.py
+// copy parameters to tb.v, ttfir.v, test.py
+// as files may be used individually
+module tb #(parameter N_TAPS = 6,
+                      BW_in = 6,
+                      BW_out = 8
+    )
+    (
     input clk,
     input rst,
-    output [6:0] segments
+    input signed [BW_in - 1:0] x_in,
+    output signed [BW_out - 1:0] y_out
    );
 
     // this part dumps the trace to a vcd file that can be viewed with GTKWave
@@ -21,12 +27,15 @@ module tb (
     end
 
     // wire up the inputs and outputs
-    wire [7:0] inputs = {6'b0, rst, clk};
+    wire [7:0] inputs;
+    assign inputs[BW_in - 1 + 2:0] = {x_in, rst, clk};
+    if (BW_in < 6)
+        assign inputs[7:BW_in + 2] = 0;
     wire [7:0] outputs;
-    assign segments = outputs[6:0];
+    assign y_out = outputs[BW_out - 1:0];
 
     // instantiate the DUT
-    seven_segment_seconds seven_segment_seconds(
+    gbsha_top gbsha_top(
         `ifdef GL_TEST
             .vccd1( 1'b1),
             .vssd1( 1'b0),
--- src/test.py
+++ src/test.py
@@ -2,22 +2,43 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+def binstr2signed_int(x):
+    bw = len(x)
+    if bw == 1:
+        return -int(x, 2)
+    return int(x[1:], 2) - int(x[0], 2) * 2**(bw - 1)
 
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
+# copy parameters to tb.v, ttfir.v, test.py
+# as files may be used individually
+N_TAPS = 6
+BW_in =  6
+BW_out = 8
+
+# test sequence for multiplication
+input =                    [1,-3, 0, 0, 0, 0, 1, 3, 4,  5,  6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+output_expected = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -3,-8,-9,-11,-13, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# # test sequence for multiplication
+# input =             [ 3, 1, 3,  4,  5,  6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# input_sign =        [ 1, 0, 0,  1,  0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# output_expected = [0, 0,-3,-9, 12,-15,-18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# # test sequence for subtraction
+# input =              [3, 1, 3,  4,  5,  6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# output_expected = [0,-3,-2, 0,  1,  2,  3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3]
 
 @cocotb.test()
-async def test_7seg(dut):
+async def test_gbsha_top(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
-
     dut._log.info("reset")
     dut.rst.value = 1
     await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
-
-    dut._log.info("check all segments")
-    for i in range(10):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, 1000)
-        assert int(dut.segments.value) == segments[i]
+    dut._log.info("checking...")
+    for i, x in enumerate(input):
+        dut.x_in.value = x
+        await ClockCycles(dut.clk, 1)
+        output_actual = binstr2signed_int(dut.y_out.value.binstr)
+        print(f"{output_actual = }, expected = {output_expected[i]}")
