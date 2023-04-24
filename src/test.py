eb698910e6481eb13f97116a6d7be57855024ffb
Elysia Love <elysia@love>
2023-04-24 10:51:22 +0200

    re-removing input register

--- src/test.py
+++ src/test.py
@@ -10,11 +10,12 @@ def binstr2signed_int(x):
     return int(x[1:], 2) - int(x[0], 2) * 2**(bw - 1)
 
 N_TAPS = 4
+INPUT_REG = 0
 
 @cocotb.test()
 async def test_delay(dut):
     input =           [0] * (N_TAPS - 1 + 1) + [8, 4, 0, 0]
-    output_expected = [0] * (N_TAPS + 2 + 1) + [1]
+    output_expected = [0] * (N_TAPS + 2 + INPUT_REG) + [1]
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -33,7 +34,7 @@ async def test_delay(dut):
 @cocotb.test()
 async def test_identity_function(dut):
     input =           [0] * (N_TAPS - 1 + 1) + [16] + [x * 2 for x in range(-8, 8)] + [0, 0]
-    output_expected = [0] * (N_TAPS + 2 + 1) + [x for x in range(-8, 8)]
+    output_expected = [0] * (N_TAPS + 2 + INPUT_REG) + [x for x in range(-8, 8)]
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -52,7 +53,7 @@ async def test_identity_function(dut):
 @cocotb.test()
 async def test_minus_function(dut):
     input =           [0] * (N_TAPS - 1 + 1) + [-31] + [x * 2 for x in range(-15, 16)] + [0, 0]
-    output_expected = [0] * (N_TAPS + 2 + 1) + [-(x * 2 * 31)//32 for x in range(-15, 16)]
+    output_expected = [0] * (N_TAPS + 2 + INPUT_REG) + [-(x * 2 * 31)//32 for x in range(-15, 16)]
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -71,7 +72,7 @@ async def test_minus_function(dut):
 @cocotb.test()
 async def test_maximum_value(dut):
     input =           [0] + [-31 for _ in range(2 * N_TAPS)] + 2 * [0]
-    output_expected = [0] * (N_TAPS + 2 + 1) + [i * 31 * 31 // 32 for i in range(1, N_TAPS + 1)]
+    output_expected = [0] * (N_TAPS + 2 + INPUT_REG) + [i * 31 * 31 // 32 for i in range(1, N_TAPS + 1)]
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
@@ -90,7 +91,7 @@ async def test_maximum_value(dut):
 @cocotb.test()
 async def test_minimum_value(dut):
     input =           [0] + [-32] * N_TAPS + [31] * N_TAPS
-    output_expected = [0] * (N_TAPS + 2 + 1) + [-32 * 31 * i // 32  for i in range(1, N_TAPS + 1)]
+    output_expected = [0] * (N_TAPS + 2 + INPUT_REG) + [-32 * 31 * i // 32  for i in range(1, N_TAPS + 1)]
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
